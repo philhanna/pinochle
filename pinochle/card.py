31d5f30bf1b8b1b1ce8e6ee4be61eaf361ed6033
Phil Hanna <ph1204@gmail.com>
2020-07-24 13:14:41 -0400

    Streamlined card parser

--- pinochle/card.py
+++ pinochle/card.py
@@ -27,77 +27,49 @@ class Card:
     def __hash__(self):
         return hash(self.rank) ^ hash(self.suit)
 
+
 class CardParser:
     """ Parses an input string for a card name """
 
     @staticmethod
     def parse(s):
-
         rank = None
-        suit = None
+        state = 0
 
-        def check_first_letter(ch):
-            nonlocal rank, suit
-            if ch == '1':
-                state = State.AFTER_1
-            elif ch not in "AKQJ9":
-                state = State.ERROR
-            else:
-                rankmap = {
-                    'A': Rank.ACE,
-                    'K': Rank.KING,
-                    'Q': Rank.QUEEN,
-                    'J': Rank.JACK,
-                    '9': Rank.NINE
-                }
-                rank = rankmap[ch]
-                state = State.AFTER_RANK
-            return state
-
-        def check_for_zero(ch):
-            nonlocal rank, suit
-            if ch == '0':
-                rank = Rank.TEN
-                state = State.AFTER_RANK
-            else:
-                state = State.ERROR
-            return state
-
-        def check_for_suit(ch):
-            nonlocal rank, suit
-            if ch not in "HCDS":
-                state = State.ERROR
-            else:
-                suitmap = {
-                    'S': Suit.SPADES,
-                    'H': Suit.HEARTS,
-                    'D': Suit.DIAMONDS,
-                    'C': Suit.CLUBS
-                }
-                suit = suitmap[ch]
-                state = State.GOT_IT
-            return state
-
-        transitions = {
-            State.INIT: check_first_letter,
-            State.AFTER_1: check_for_zero,
-            State.AFTER_RANK: check_for_suit
-        }
-
-        state = State.INIT
         for ch in s.upper():
-            state = transitions[state](ch)
-            if state == State.ERROR:
-                return None
-            if state == State.GOT_IT:
-                return Card(rank, suit)
-        return None
+
+            if state == 0:
+                if ch == '1':
+                    state = 1
+                elif ch not in "AKQJ9":
+                    return None
+                else:
+                    rank = rankmap[ch]
+                    state = 2
+
+            elif state == 1:
+                if ch == '0':
+                    rank = Rank.TEN
+                    state = 2
+                else:
+                    return None
+
+            elif state == 2:
+                if ch not in "HCDS":
+                    return None
+                return Card(rank, suitmap[ch])
 
 
-class State(IntEnum):
-    """ Used in the parse method """
-    INIT = 0
-    AFTER_1 = 1
-    AFTER_RANK = 2
-    ERROR = 3
-    GOT_IT = 4
+rankmap = {
+    "A": Rank.ACE,
+    "K": Rank.KING,
+    "Q": Rank.QUEEN,
+    "J": Rank.JACK,
+    "9": Rank.NINE
+}
+suitmap = {
+    'H': Suit.HEARTS,
+    'C': Suit.CLUBS,
+    'D': Suit.DIAMONDS,
+    'S': Suit.SPADES
+}
