4dad41f5cc2a4a54f863ee0e35370597835d8737
Phil Hanna <ph1204@gmail.com>
2020-07-24 00:34:39 -0400

    Added unit test for str(hand)

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -5,16 +5,25 @@ from pinochle import Hand, Deck, Suit
 
 class TestHand(TestCase):
 
-    def test_organized(self):
+    def setUp(self):
         deck = Deck()
         deck.shuffle()
         hand = Hand()
         for i, card in enumerate(deck.cards):
             if i % 4 == 0:
                 hand.add(card)
-        orgmap = hand.organized()
+        self.hand = hand
+
+    def test_organized(self):
+        orgmap = self.hand.organized()
         for suitname, cardlist in orgmap.items():
             self.assertIn(suitname, [suit.value for suit in Suit])
             self.assertGreater(len(cardlist), 0)
             for card in cardlist:
                 self.assertEqual(card.suit.value, suitname)
+
+    def test_str(self):
+        actual = str(self.hand)
+        # All we can do here is check that at least one suit name
+        # is mentioned in the string
+        self.assertTrue(any([suit.value in actual for suit in Suit]))
