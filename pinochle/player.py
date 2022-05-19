9fdfb04bc3e9d6301e2aa610ad44ba88cf673966
Phil Hanna <ph1204@gmail.com>
2022-05-19 12:19:05 -0400

    Added partner attribute to player

--- pinochle/player.py
+++ pinochle/player.py
@@ -1,12 +1,26 @@
-from pinochle import CardChoosingStrategy
+from pinochle import CardChoosingStrategy, Hand
 
 
 class Player:
     """ One of the four players in the game """
+
     def __init__(self, name):
-        self.name = name
-        self.card_choosing_strategy = CardChoosingStrategy()
-        self.hand = None
+        self._name : str = name
+        self._partner : Player = None
+        self._card_choosing_strategy : CardChoosingStrategy = CardChoosingStrategy()
+        self._hand : Hand = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def partner(self):
+        return self._partner
+
+    @partner.setter
+    def partner(self, value):
+        self._partner = value
 
     @property
     def hand(self):
@@ -18,16 +32,16 @@ class Player:
 
     def choose_card(self, cards):
         """ Chooses a card from a list """
-        return self.card_choosing_strategy.choose_card(cards)
+        return self._card_choosing_strategy.choose_card(cards)
 
     def __repr__(self):
-        return f"{__class__.__name__}(\"{self.name}\")"
+        return f"{__class__.__name__}(\"{self._name}\")"
 
     def __str__(self):
-        return self.name
+        return self._name
 
     def hash(self):
-        return hash(self.name)
+        return hash(self._name)
 
     def __eq__(self, other):
         return self.name == other.name
