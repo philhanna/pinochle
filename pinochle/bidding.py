f0a245cfee1496f1dfc6d2ca23bc5c8af49d8bbb
Phil Hanna <ph1204@gmail.com>
2022-05-19 12:39:24 -0400

    Reformatted docstring

--- pinochle/bidding.py
+++ pinochle/bidding.py
@@ -10,8 +10,10 @@ class Bidding:
     Bidding starts with the player to the dealer's left.
     Opening bid must be 250 or greater.
     Subsequent bids must be either:
+
     - Pass, or
     - A multiple of 10 that is greater than the previous bid
+
     The bidding ends when all but one player has passed.
     If all four players pass on the first go around, the
     round is canceled.
