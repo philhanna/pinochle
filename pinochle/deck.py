f52a4514d43924e15beae87fc0d1dfe719f79d3d
Phil Hanna <ph1204@gmail.com>
2022-07-11 17:19:01 -0400

    Added type annotations to Deck

--- pinochle/deck.py
+++ pinochle/deck.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from typing import List
 
 from pinochle import Suit, Rank, Card
 
@@ -6,14 +7,14 @@ from pinochle import Suit, Rank, Card
 class Deck:
     """ The basic pinochle deck """
     def __init__(self):
-        self.cards = [Card(rank, suit) for rank in Rank for suit in Suit]*2
+        self.cards: List[Card] = [Card(rank, suit) for rank in Rank for suit in Suit]*2
 
     @property
-    def cards(self):
+    def cards(self) -> List[Card]:
         return self._cards
 
     @cards.setter
-    def cards(self, value):
+    def cards(self, value: List[Card]):
         self._cards = value
 
     def shuffle(self):
