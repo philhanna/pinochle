d5a091ee76af94177bbf65dca83aabd6ee84efc5
Phil Hanna <ph1204@gmail.com>
2022-07-11 17:03:03 -0400

    Added unit test for player

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
 
-from pinochle import CardParser
+from pinochle import CardParser, Player, Card
 
 
 class TestPlayer(TestCase):
 
     def test_choose_card(self):
         cards = [CardParser.parse(cardname) for cardname in ["As", "10d", "QS", "JD"]]
-        print(cards)
+        player: Player = Player("Dad")
+        card: Card = player.choose_card(cards)
+        self.assertIn(card, cards)
