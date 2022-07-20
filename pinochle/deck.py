80770a6115263c0f8468a5602ce963bed940e850
Phil Hanna <ph1204@gmail.com>
2022-07-19 23:38:27 -0400

    Added shuffle() method to Deck

--- pinochle/deck.py
+++ pinochle/deck.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 from pinochle import Card, Rank, Suit
@@ -8,8 +9,10 @@ class Deck:
     def  __init__(self):
         """ The basic pinochle deck """
         self._cards: List = [Card(rank, suit) for rank in Rank for suit in Suit] * 2
-        print(self._cards)
 
     @property
     def cards(self):
         return self._cards
+
+    def shuffle(self):
+        random.shuffle(self._cards)
