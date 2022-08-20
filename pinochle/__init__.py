572af66b5abbc9f968dd24c4e438a1d599879249
Phil Hanna <ph1204@gmail.com>
2022-08-19 21:09:10 -0400

    Moved __all__ to end of __init__.py file

--- pinochle/__init__.py
+++ pinochle/__init__.py
@@ -1,22 +1,9 @@
-# Pinochle rules: https://en.wikipedia.org/wiki/Pinochle
-
 from random import seed as SEED
 from .suit import Suit
 from .rank import Rank
 from .card import Card, CardParser
 from .player import Player
 from .deck import Deck
-
-__all__ = [
-    'set_seed',
-    "Suit",
-    "Rank",
-    "Card",
-    "CardParser",
-    "Player",
-    'PASS',
-    "Deck"
-]
 
 # Define a constant for a bid of PASS
 PASS = -1
@@ -29,3 +16,13 @@ def set_seed(x: int | None):
 
 set_seed(None)
 
+__all__ = [
+    'set_seed',
+    "Suit",
+    "Rank",
+    "Card",
+    "CardParser",
+    "Player",
+    'PASS',
+    "Deck"
+]
