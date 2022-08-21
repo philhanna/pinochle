386fa98c0e9e8610179be71d0ad721873be19373
Phil Hanna <ph1204@gmail.com>
2022-08-21 17:37:55 -0400

    Changed player name

--- tests/test_player.py
+++ tests/test_player.py
@@ -6,12 +6,12 @@ from pinochle import Player, CardParser, Card, Rank, Suit
 class TestPlayer(TestCase):
 
     def test_init(self):
-        player1 = Player("John")
-        player2 = Player("John")
+        player1 = Player("John Doe")
+        player2 = Player("John Doe")
         self.assertEqual(player1.name, player2.name)
 
     def test_add_card_to_hand(self):
-        player = Player("John")
+        player = Player("John Doe")
         player.add_card_to_hand(CardParser.parse("Queen of Spades"))
         player.add_card_to_hand(CardParser.parse("Jack of Diamonds"))
         actual: list[Card] = player.cards
