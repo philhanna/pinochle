3730f74aa1cdc6ba5f78788be0f8becf12742a46
Phil Hanna <ph1204@gmail.com>
2022-12-19 09:22:06 -0500

    Converted test_card.py to pytest

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,25 +1,24 @@
-from unittest import TestCase
-
 from pinochle import Card, Rank, Suit, CardParser
 
 
-class TestCard(TestCase):
+def test_constructor():
+    rank: Rank = Rank.TEN
+    suit: Suit = Suit.DIAMONDS
+    card: Card = Card(rank, suit)
+    card_name = str(card)
+    assert "10 of Diamonds" == card_name
 
-    def test_constructor(self):
-        rank: Rank = Rank.TEN
-        suit: Suit = Suit.DIAMONDS
-        card: Card = Card(rank, suit)
-        card_name = str(card)
-        self.assertEqual("10 of Diamonds", card_name)
 
-    def test_parser(self):
-        card: Card = CardParser.parse("10 of Diamonds")
-        self.assertEqual("10 of Diamonds", str(card))
+def test_parser():
+    card: Card = CardParser.parse("10 of Diamonds")
+    assert "10 of Diamonds" == str(card)
 
-    def test_parser_from_card_class(self):
-        card: Card = Card.parse("9 of clubs")
-        self.assertEqual("Nine of Clubs", str(card))
 
-    def test_repr(self):
-        card: Card = Card.parse("9 of clubs")
-        self.assertEqual("Nine of Clubs", repr(card))
+def test_parser_from_card_class():
+    card: Card = Card.parse("9 of clubs")
+    assert "Nine of Clubs" == str(card)
+
+
+def test_repr():
+    card: Card = Card.parse("9 of clubs")
+    assert "Nine of Clubs" == repr(card)
