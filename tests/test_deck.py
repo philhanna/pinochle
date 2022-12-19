d8bef94063bc44bfa19a7e1e98fa920eddf847b6
Phil Hanna <ph1204@gmail.com>
2022-12-19 09:23:30 -0500

    Converted test_deck.py to pytest

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,16 +1,13 @@
-from unittest import TestCase
-
 from pinochle import Deck
 
 
-class TestDeck(TestCase):
+def test_cards():
+    deck = Deck()
+    assert 48 == len(deck.cards)
 
-    def test_cards(self):
-        deck = Deck()
-        self.assertEqual(48, len(deck.cards))
 
-    def test_shuffle(self):
-        deck = Deck()
-        deck.shuffle()
-        self.assertEqual(48, len(deck.cards))
+def test_shuffle():
+    deck = Deck()
+    deck.shuffle()
+    assert 48 == len(deck.cards)
 
