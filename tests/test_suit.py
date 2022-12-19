67a0ef0629aacb7cca86746f4dda9b8f0c14cf11
Phil Hanna <ph1204@gmail.com>
2022-12-19 09:40:27 -0500

    Converted test_suit.py to pytest

--- tests/test_suit.py
+++ tests/test_suit.py
@@ -1,26 +1,13 @@
-from unittest import TestCase
+import pytest
 
 from pinochle import Suit
 
 
-class TestSuit(TestCase):
-
-    def test_order_hearts(self):
-        expected: str = "Hearts"
-        actual: str = str(Suit.HEARTS)
-        self.assertEqual(expected, actual)
-
-    def test_order_clubs(self):
-        expected: str = "Clubs"
-        actual: str = str(Suit.CLUBS)
-        self.assertEqual(expected, actual)
-
-    def test_order_diamonds(self):
-        expected: str = "Diamonds"
-        actual: str = str(Suit.DIAMONDS)
-        self.assertEqual(expected, actual)
-
-    def test_order_spades(self):
-        expected: str = "Spades"
-        actual: str = str(Suit.SPADES)
-        self.assertEqual(expected, actual)
+@pytest.mark.parametrize("name,suit", [
+    ("Hearts", Suit.HEARTS),
+    ("Clubs", Suit.CLUBS),
+    ("Diamonds", Suit.DIAMONDS),
+    ("Spades", Suit.SPADES),
+])
+def test_suit(name, suit):
+    assert name == str(suit)
