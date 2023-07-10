56ca1beb577717613aea5038b6836c78f0404c0b
Phil Hanna <ph1204@gmail.com>
2023-07-10 06:34:21 -0400

    Starting Go version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/philhanna/pinochle
+
+go 1.20
--- pinochle/controller.py
+++
@@ -1,7 +0,0 @@
-class Controller:
-    """ The controller of the game.
-    Uses sockets to add players, start games, keep score, etc.
-    """
-    def __init__(self):
-        """ Creates a new Administrator """
-        pass
