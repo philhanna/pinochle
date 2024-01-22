fcb715e7a4445a70b16a8e73aeaee5d338c091af
Phil Hanna <ph1204@gmail.com>
2024-01-22 13:52:48 -0500

    Added subtypes of Player

--- player.go
+++ player.go
@@ -1,5 +1,19 @@
 package pinochle
 
+// Player is one of the four participants in the game.
 type Player struct {
-	
+	ID       string
+	Name     string
+	Position uint8
+	TeamID   string
+}
+
+type HumanPlayer struct {
+	Player
+	Host string
+	Port int
+}
+
+type ComputerPlayer struct {
+	Player
 }
