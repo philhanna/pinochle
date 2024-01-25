512c5d14c6a898df7b3cd06a2d0d7c360343fa0c
Phil Hanna <ph1204@gmail.com>
2024-01-24 22:03:45 -0500

    Added comment

--- deck.go
+++ deck.go
@@ -15,6 +15,10 @@ type Deck struct {
 	cards []Card
 }
 
+// ---------------------------------------------------------------------
+// Constructor
+// ---------------------------------------------------------------------
+
 // NewDeck creates a new 48-card pinochle deck
 func NewDeck() Deck {
 	cards := make([]Card, 0)
