b242afc12647d32a3bf77c0b32a11d68c7bdb244
Phil Hanna <ph1204@gmail.com>
2025-03-15 23:35:57 -0400

    Whitespace only

--- tests/test_ranks.py
+++ tests/test_ranks.py
@@ -2,6 +2,7 @@ import pytest
 
 from cards import Rank
 
+
 @pytest.mark.parametrize(("rank1, rank2, expected"), [
     (Rank.NINE, Rank.QUEEN, False),
     (Rank.TEN, Rank.JACK, True),
@@ -11,19 +12,20 @@ from cards import Rank
 def test_greater_than(rank1, rank2, expected):
     actual = rank1.value > rank2.value
     assert actual == expected
-    
+
+
 @pytest.mark.parametrize(("rank", "expected"), [
     (Rank.NINE, "9"),
     (Rank.JACK, "J"),
-    (Rank.QUEEN,"Q"),
-    (Rank.KING,"K"),
-    (Rank.TEN,"10"),
-    (Rank.ACE,"A"),
+    (Rank.QUEEN, "Q"),
+    (Rank.KING, "K"),
+    (Rank.TEN, "10"),
+    (Rank.ACE, "A"),
 ])
 def test_short_name(rank, expected):
     actual = rank.short_name()
     assert actual == expected
-    
+
 
 @pytest.mark.parametrize(("rank", "expected"), [
     (Rank.NINE, "9"),
